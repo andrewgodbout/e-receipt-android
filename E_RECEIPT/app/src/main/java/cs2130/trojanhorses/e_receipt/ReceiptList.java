96cbe796f4593d5a29b30925bc426b0becd2cfb9
davadams <31736142+davadams@users.noreply.github.com>
2017-10-21 01:03:58 -0300

    Changes made - David

---
+++ E_RECEIPT/app/src/main/java/cs2130/trojanhorses/e_receipt/ReceiptList.java
@@ -0,0 +1,12 @@
+package cs2130.trojanhorses.e_receipt;
+
+/**
+ * Created by davidadams on 2017-10-21.
+ */
+
+public class ReceiptList {
+
+    private ReceiptList{
+
+    }
+}
--- E_RECEIPT/app/src/main/java/cs2130/trojanhorses/e_receipt/ReceiptPagerActivity.java
+++ E_RECEIPT/app/src/main/java/cs2130/trojanhorses/e_receipt/ReceiptPagerActivity.java
@@ -1,11 +1,30 @@
 package cs2130.trojanhorses.e_receipt;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 /**
  * Created by davidadams on 2017-10-21.
  */
 
 public class ReceiptPagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Receipt> mReceipts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.receipt_view_pager);
+
+        mReceipts = ReceiptList.get(this).getReceipts();
+
+    }
+
 
 }
